d04ee584019a9f64386441efbd21ad65ab9229e2
erpreciso <trepreciso@gmail.com>
2024-02-20 12:27:39 +0100

    Add test.

---
+++ test/test.py
@@ -0,0 +1,35 @@
+import sys
+import os
+import pdb
+import unittest
+
+test_path = os.path.dirname(__file__)
+parent = os.path.abspath(os.path.join(test_path, os.pardir))
+sys.path.append(parent)
+
+
+class TestMatches(unittest.TestCase):
+    def setUp(self):
+        spreadsheet_path = os.path.join(test_path, "categorizer.ods")
+        sheet_name = "Sheet1"
+        from spreadsheet_categorizer import SpreadsheetCategorizer as SC
+        self.sc = SC(spreadsheet_path, sheet_name)
+
+    def test_match_payee_desc_case_insensitive(self):
+        self.assertEqual(self.sc.match('gas station', 'tank car #1'),
+                         ('Expenses:Car:Volvo', 'Assets:Bank'))
+        self.assertEqual(self.sc.match('GAS station', 'tank CAR #1'),
+                         ('Expenses:Car:Volvo', 'Assets:Bank'))
+        self.assertEqual(self.sc.match('Gas Station', 'Tank Car #2'),
+                         ('Expenses:Car:Saab', 'Assets:Bank'))
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+
+if __name__ == '__main__':
+    unittest.main()
